89b08f352ab2d0daa3485fb58ef2bea290c25a31
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-17 09:57:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+        .then(resp => resp.json())
+        .then(function(obj) {
+            const p = document.createElement('p');
+            p.textContent = obj.id;
+            document.querySelector('body').appendChild(p);
+        })
+        .catch(function(error) {
+            const p = document.createElement('p');
+            p.textContent = error.message;
+            document.querySelector('p').appendChild(p);
+        });
+};
